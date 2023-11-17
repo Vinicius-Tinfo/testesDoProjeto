55e9193b6acd51df59a4cc972c11fbe31f228764
vinicius <ALUNO@ETEJK-LAB04-M07>
2023-11-16 21:33:09 -0300

    adição

---
+++ mais_saude_teste/src/main/resources/static/vinicius-mudancas.js
@@ -0,0 +1,36 @@
+/*Codigo para incluir um html em outro  */
+/* na div você colocar como abaixo 
+<div w3-include-html="h1.html">
+<div nome do metodo= caminho do html*/
+function includeHTML() {
+    var z, i, elmnt, file, xhttp;
+    /* Loop through a collection of all HTML elements: */
+    z = document.getElementsByTagName("*");
+    for (i = 0; i < z.length; i++) {
+        elmnt = z[i];
+        /*search for elements with a certain atrribute:*/
+        file = elmnt.getAttribute("w3-include-html");
+        if (file) {
+            /* Make an HTTP request using the attribute value as the file name: */
+            xhttp = new XMLHttpRequest();
+            xhttp.onreadystatechange = function () {
+                if (this.readyState == 4) {
+                    if (this.status == 200) { elmnt.innerHTML = this.responseText; }
+                    if (this.status == 404) { elmnt.innerHTML = "Page not found."; }
+                    /* Remove the attribute, and call this function once more: */
+                    elmnt.removeAttribute("w3-include-html");
+                    includeHTML();
+                }
+            }
+            xhttp.open("GET", file, true);
+            xhttp.send();
+            /* Exit the function: */
+            return;
+        }
+    }
+}
+/* no final da pagina tem que acionar 
+    <script>
+    includeHTML();
+    </script> 
+     */
